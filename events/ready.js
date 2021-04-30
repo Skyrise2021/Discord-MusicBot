be5c6e154390f9f4d6062a57c7b82c6dd5bc65d8
Skyrise2021 <80463391+Skyrise2021@users.noreply.github.com>
2021-04-30 18:07:46 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity("m.help By Relzz", {
+    type: "WATCHING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
