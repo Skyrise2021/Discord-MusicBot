0f670d9e588d89f39229be44c562e564b9b87be2
Skyrise2021 <80463391+Skyrise2021@users.noreply.github.com>
2021-04-30 17:02:10 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity("m.help | VISORS FAMILY |", {
+    type: "STREAMING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
